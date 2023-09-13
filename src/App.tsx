75e854a2ac03160851ea4ce68894e5908d790dd0
Nur Mohammad <mohammadnur5480@gmail.com>
2023-09-13 07:10:34 +0600

    Add: create a Button component

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Button from './Components/Button'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full flex justify-center items-center'>
+      <Button color='success' variant='filled'>Button</Button>
+    </div>
+  )
+}
+export default App
---
+++ src/Components/Button.tsx
@@ -0,0 +1,41 @@
+import {ReactNode} from 'react';
+
+type ButtonProps ={
+    children:ReactNode
+    color?:'primary' | 'secondary' | 'success'
+    variant?:'filled' | 'outline'
+};
+
+type IColor = {
+    primary: string;
+    success: string;
+    secondary: string;
+};
+
+type IVariants ={
+    filled: string;
+    outline: string;
+};
+
+const Button = ({children,color='primary',variant='filled'}:ButtonProps) => {
+
+    const colors:IColor = {
+        primary:
+          "bg-blue-600 hover:bg-blue-600 hover:text-white text-white  border-blue-600",
+        success:
+          "bg-green-500 hover:bg-green-600 hover:text-white text-white border-green-600",
+          secondary:
+          "bg-yellow-500 hover:bg-yellow-600 hover:text-white text-white border-yellow-600",
+      };
+ 
+      const variants:IVariants = {
+        filled: "",
+        outline: "border bg-transparent text-black",
+      };
+  return (
+    <button className={`${colors[color]} ${variants[variant]} font-semibold py-2 px-6 rounded-md`}>
+        {children}
+    </button>
+  )
+}
+export default Button;
