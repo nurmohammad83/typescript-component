016f8ec319725ba4e934774d817d3d5033c99bdf
Nur Mohammad <mohammadnur5480@gmail.com>
2023-09-15 10:36:54 +0600

    fix: about page style

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,6 @@
 const About = () => {
   return (
-    <div>About</div>
+    <div className="text-3xl h-screen text-blue-500 font-semibold py-16 text-center">About Coming Soon...</div>
   )
 }
 export default About
